a6e8ee7892edd1374da800f87bd0a68aea2ba4c3
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-09-10 23:01:28 -0700

    ok i gotta start over

--- main.py
+++ main.py
@@ -3,9 +3,64 @@ from simulator import Simulator, centerline
 
 sim = Simulator()
 
-prev_error = np.zeros(5)
-total_error = np.zeros(5)
-period = 0.02 # don't know what this is but should be how often the control loop runs
+x_kp = 1
+x_ki = 0
+x_kd = 0
+
+y_kp = 1
+y_ki = 0
+y_kd = 0
+
+theta_kp = 1
+theta_ki = 0
+theta_kd = 0
+
+meters = np.zeros(105)
+for i in range(105): # 105 meter track
+    meters[i] = i
+path = centerline(meters)
+
+dist_traveled = 0
+x_last = -1
+y_last = -1
+
+# def find_next_point(x):
+#     for p in path:
+#         if p[0] > x:
+#             return p
+#     return np.zeros(2)
+
+def find_nearest_point(pt):
+    min_dist = np.linalg.norm(pt.subtract(path[0]))
+    min_pt = path[0]
+    index = 0
+    for i in range(105):
+        if np.linalg.norm(pt.subtract(path[i])) < min:
+            min_pt = path[i]
+            index = i
+    return min_pt, i
+
+speed = 4
+class PIDController():
+    total_error = 0
+    prev_error = 0
+    period = 0.02 # don't know what this is but should be how often the control loop runs
+
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+    def calculate(self, current, setpoint):
+        error = setpoint - current
+        output = self.kp * error + self.ki * self.total_error + self.kd * ((error - self.prev_error) / self.period)
+        self.prev_error = error
+        self.total_error += error
+        return output
+
+x_controller = PIDController(x_kp, x_ki, x_kd)
+y_controller = PIDController(y_kp, y_ki, y_kd)
+theta_controller = PIDController(theta_kp, theta_ki, theta_kd)
 
 def controller(x):
     """controller for a car
@@ -22,32 +77,18 @@ def controller(x):
     v      = x[3]                   # current velocity
     theta   = x[4]                  # current steering angle
 
-    # TODO tune
-    # this is probably different for xy and angle actually 
-    kp = 0
-    ki = 0
-    kd = 0
+    # we are starting over actually
 
-    setpoint = np.zeros(5) #TODO how do you calculate the setpoint??
-    output = np.zeros(5)
+    nearest_pt = find_nearest_point(np.array[xpos, ypos])[0]
+    index = find_nearest_point(np.array[xpos, ypos])[1]
+    setpoint = path[index + 1]
+    x_vel = np.cos(phi) * v
+    y_vel = np.sin(phi) * v
+    x_vel += x_controller.calculate(xpos, nearest_pt[0])
+    y_vel += y_controller.calculate(ypos, nearest_pt[1])
 
-    # iterate 3x to find error and output for xpos, ypos, and phi
-    for i in range(3):
-        error = setpoint[i] - x[i]
-        total_error[i] += error
-        output[i] = kp * error + ki * total_error[i] + kd * ((error - prev_error[i]) / period)
-        prev_error[i] = error
-
-    # find difference between current vel and output vel, divide by period to get accel
-    a = (v - np.linalg.norm(output[0], output[1])) / period
-    '''
-    find difference between current steering wheel angle and desired angle (?)
-    f.e. if you want to be at 10 degrees and are currently at 0 (10 deg error) but your wheel is already at 10 degrees, don't do anything
-    by contrast if you want to be at 0 and are currently at 10 degrees (-10 deg error) and your wheel is at like idk 5 degrees
-    you should go -15 degrees
-    this doesn't seem right lol
-    '''
-    d_theta = (output[3] - theta) / period
+    
+    
     return np.array([a, d_theta])
 
 sim.set_controller(controller)
